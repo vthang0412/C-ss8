c00b8b9064f04c02170306cfae555c06d5feefde
vthang0412 <vthang041206@gmail.com>
2024-11-28 23:30:36 +0700

    Add files via upload

---
+++ ss8-ex6.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Nhap kich thuoc cua ma tran vuong: ");
+    scanf("%d", &n);
+	    int a[n][n];
+	    printf("Nhap cac phan tu cua ma tran:\n");
+	    for (int i = 0; i < n; i++) {
+	        for (int j = 0; j < n; j++) {
+	            printf("Nh?p ph?n t? a[%d][%d]: ", i, j);
+	            scanf("%d", &a[i][j]);
+	        }
+	    }
+		    int sum = 0;
+		    printf("Cac phan tu tren duong cheo: ");
+		    for (int i = 0; i < n; i++) {
+		        printf("%d ", a[i][i]);  
+		        sum += a[i][i];         
+		    }
+    printf("\nTong cac phan tu tren duong cheo: %d\n", sum);
+    return 0;
+}
+
