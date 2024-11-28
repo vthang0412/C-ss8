9a0b44e2b2d9c5716e0411471b7ecf71a492b8a1
vthang0412 <vthang041206@gmail.com>
2024-11-28 23:28:49 +0700

    Update ss8-ex3.cpp

--- ss8-ex3.cpp
+++ ss8-ex3.cpp
@@ -4,21 +4,20 @@ int main() {
     int n;
     printf("Nhap vao mot so nguyen: ");
     scanf("%d", &n);
-    int arr[n][n];
-    printf("Nhap vao cac phan tu cua mang:\n");
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            printf("Nhap phan tu arr[%d][%d]: ", i, j);
-            scanf("%d", &arr[i][j]);
-        }
-    }
-    printf("Ma tran vuong la:\n");
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            printf("%d ", arr[i][j]);
-        }
-        printf("\n");
-    }
-    return 0;
+	    int arr[n][n];
+	    printf("Nhap vao cac phan tu cua mang:\n");
+	    for (int i = 0; i < n; i++) {
+	        for (int j = 0; j < n; j++) {
+	            printf("Nhap phan tu arr[%d][%d]: ", i, j);
+	            scanf("%d", &arr[i][j]);
+	        }
+	    }
+			    printf("Ma tran vuong la:\n");
+			    for (int i = 0; i < n; i++) {
+			    for (int j = 0; j < n; j++) {
+			        printf("%d ", arr[i][j]);
+			        }
+			        printf("\n");
+			    }
+			    return 0;
 }
-
